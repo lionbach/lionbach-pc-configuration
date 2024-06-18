f6cb1e320b62a4442e6cef6233b3a86f097f07f4
Leo Mumbach <leomumbach@gmail.com>
2024-06-18 01:17:34 -0300

    minor fixed

--- script/install-software-dev/install FortiClient VPN (.deb).sh
+++ script/install-software-dev/install FortiClient VPN (.deb).sh
@@ -2,4 +2,5 @@
 wget -O temp.deb "https://filestore.fortinet.com/forticlient/forticlient_vpn_7.4.0.1636_amd64.deb"
 sudo gdebi -n ./temp.deb
 rm temp.deb
+sudo systemctl stop forticlient
 sudo systemctl disable forticlient
--- script/install-software/install Grub White Sur Theme.sh
+++ script/install-software/install Grub White Sur Theme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone git@github.com:vinceliuice/grub2-themes.git
 cd grub2-themes
-sudo ./install.sh -t whitesur 
+sudo ./install.sh -t stylish
 cd ..
 rm -rf grub2-themes
